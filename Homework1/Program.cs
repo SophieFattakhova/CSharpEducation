d2d0761facf7bd343265cfc704eac8d86df0457c
SophieFattakhova <fattakhovag.f@gmail.com>
2024-04-05 10:59:19 +0500

    Домашняя работа №1

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Программа №1
+Необходимо создать консольное приложение, в котором пользователь может ввести свое имя, фамилию и дату рождения.
+Эти данные пользователь должен вводить по очереди.
+В ответ на введённые данные пользователь получает строку с их представлением.
+ */
+
+Console.WriteLine("Введите имя: ");
+string? name = Console.ReadLine();
+
+Console.Write("Введите фамилию: ");
+string? lastName = Console.ReadLine();
+
+Console.Write("Введите дату рождения: ");
+string? birthdate = Console.ReadLine();
+
+Console.WriteLine();
+
+Console.WriteLine("Данные пользователя: " + $"\n{name}; {lastName}; {birthdate};"); //интерполяция и конкатениция строк.
+
+Console.WriteLine();
