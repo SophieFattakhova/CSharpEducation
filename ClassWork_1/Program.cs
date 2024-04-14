329cb1b1ffc100d152a4904fb50154b2a74f05b8
SophieFattakhova <fattakhovag.f@gmail.com>
2024-04-14 13:54:34 +0500

    Классная работа №1

---
+++ ClassWork_1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int monday = 1;
+int wednesday = 3;
+int day = 5;
+
+if (day == monday)
+{ Console.WriteLine("Сегодня понедельник!");}
+else { Console.WriteLine("Сегодня не понедельник");}
+
+if (day == monday || day == wednesday)
+{ Console.WriteLine("Сегодня день мороженого!"); }
+
+//тернарное выражение
+var result = day == monday ? "Сегодня понедельник!" : "Cегодня не понедельник!";
+Console.WriteLine(result);
